43e9edf9e9a9acd2b3ef50352524cf6833d75fd9
MerenovaAnastasiya <merenaas@yandex.ru>
2022-04-07 22:20:14 +0300

    add Remove Element task

---
+++ src/main/java/com.merenaas/easy/RemoveElement.java
@@ -0,0 +1,18 @@
+package com.merenaas.easy;
+
+public class RemoveElement {
+
+//    https://leetcode.com/problems/remove-element/
+
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int k = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val) {
+                    nums[k++] = nums[i];
+                }
+            }
+            return k;
+        }
+    }
+}
