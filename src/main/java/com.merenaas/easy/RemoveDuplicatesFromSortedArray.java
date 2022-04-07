881cfd08d038f9e572e276a8b22b7ddede9f4ab9
MerenovaAnastasiya <merenaas@yandex.ru>
2022-04-07 22:41:38 +0300

    add Remove duplicates task

---
+++ src/main/java/com.merenaas/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+package com.merenaas.easy;
+
+public class RemoveDuplicatesFromSortedArray {
+
+//    https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int k = 1;
+            if (nums.length == 1)
+                return k;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i-1])
+                    nums[k++] = nums[i];
+            }
+            return k;
+        }
+    }
+}
